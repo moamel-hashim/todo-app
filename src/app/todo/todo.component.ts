b19a7abb94aab9053f40550d6c1eea254eb38f12
moamel-hashim <muamal1997@gmail.com>
2023-11-11 15:32:27 -0800

    completed todo add

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  todo: Todo = {
+    id: 1,
+    name: "take 5 min break"
+  }
+  constructor() {}
+  ngOnInit(): void {
 
+  }
 }
