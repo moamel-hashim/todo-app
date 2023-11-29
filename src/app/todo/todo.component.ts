0de3a4c98f4341a5576878ba7dfd4e55d106448f
moamel-hashim <muamal1997@gmail.com>
2023-11-28 23:55:16 -0800

    completed assignment

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../todo';
+import { TODOS } from '../mock-todos';
 
 @Component({
   selector: 'app-todo',
@@ -7,12 +8,19 @@ import { Todo } from '../todo';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  todo: Todo = {
-    id: 1,
-    name: "take 5 min break"
-  }
+  todos = TODOS;
+  selectedTodo!: Todo;
   constructor() {}
   ngOnInit(): void {
-
+  }
+  onSelect(todo:Todo): void {
+    this.selectedTodo = todo;
+  }
+  add(name:string): void {
+    name = name.trim();
+    if(!name) {
+      return;
+    }
+    this.todos.push({id:(this.todos.length +1), name: name});
   }
 }
